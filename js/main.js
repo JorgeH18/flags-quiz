8446e1df62ebd2f45dcdc1663c7199b01a3ff78c
jorgeh18 <jorgehsmm@hotmail.com>
2023-11-26 16:20:27 +0100

    basic html css for web

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function showUserInfo(user) {
+    document.getElementById('userSelector').style.display = 'none';
+    document.getElementById('userInfo').style.display = 'block';
+    document.getElementById('userName').innerText = user;
+
+    // Show the new section
+    document.getElementById('newSection').style.display = 'block';
+}
+
+function alertshow() {
+    alert("aaaaaaaaaaaa");
+}
